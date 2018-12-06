e19cc95fabfe8b2b95889cedf5b8909cb39a5009
Jenna Mills <superangelfish555@gmail.com>
2018-12-06 07:52:50 -0700

    Deleted default view controller and launch screen storyboard.

---
+++ SuperDuperAbstractionApp/Controller/AbstractionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AbstractionViewController.swift
+//  SuperDuperAbstractionApp
+//
+//  Created by Mills, Jenna on 12/6/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class AbstractionViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
